ad1c587d01746de19ddf67a3e37c65ace8419bd9
kapil.dev <kapil.dev@netsmartz.net>
2026-01-21 17:26:38 +0530

    Add basic role and permission system scaffolding
    Introduce Permission, Role, RolePermission, and UserRole entities to support many-to-many relationships for RBAC. Update User entity to manage roles. Scaffold CreateRole feature classes and add EF Core configurations for new entities. Prepare project structure for future role and permission management features.

---
+++ src/WorkMan/WORKMAN.Auth/Infrastructure/Persistence/Configurations/PermissionConfiguration .cs
@@ -0,0 +1,29 @@
+﻿namespace WORKMAN.Auth.Infrastructure.Persistence.Configurations
+{
+    public class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            builder.ToTable("permissions");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Code)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            builder.HasIndex(x => x.Code)
+                .IsUnique();
+
+            builder.HasKey(x => new { x.Id, x.Id });
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.RolePermissions)
+                .HasForeignKey(x => x.RoleId);
+
+            builder.HasOne(x => x.Permission)
+                .WithMany()
+                .HasForeignKey(x => x.PermissionId);
+        }
+    }
+}
---
+++ src/WorkMan/WORKMAN.Auth/Infrastructure/Persistence/Configurations/RoleConfiguration .cs
@@ -0,0 +1,30 @@
+﻿
+namespace WORKMAN.Auth.Infrastructure.Persistence.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("roles");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasIndex(x => x.Name)
+                .IsUnique();
+
+            builder.HasKey(x => new { x.Id, x.Id });
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.UserRoles)
+                .HasForeignKey(x => x.UserId);
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.UserRoles)
+                .HasForeignKey(x => x.RoleId);
+        }
+    }
+}
